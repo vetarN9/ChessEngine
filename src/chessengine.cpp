aa141ef22ae87b6b7e4c275e37a91465ec87baf7
Alexander <c20alm@cs.umu.se>
2023-07-27 10:51:20 +0200

    Added test to verify that the move gen works

--- src/chessengine.cpp
+++ src/chessengine.cpp
@@ -5,20 +5,12 @@
 #include "bitboard.h"
 #include "movegen.h"
 #include "perft.h"
+#include "test.h"
 
 using namespace ChessEngine;
 
 int main(int argc, char* argv[])
 {
     Bitboards::init();
- 
-    Position pos;
-    PosInfo posInfo;
-
-    pos.Set(startPosFEN, &posInfo);
-    //pos.Set("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", &posInfo);
-
-    pos.Print();
-
-    Perft::go(pos, 6);
+    Test::perft();
 }
---
+++ src/test.h
@@ -0,0 +1,14 @@
+#ifndef TEST_INCLUDED
+#define TEST_INCLUDED
+
+namespace ChessEngine {
+
+namespace Test {
+
+void perft();
+
+} // namespace Test
+
+} // namespace ChessEngine
+
+#endif // TEST_INCLUDED
